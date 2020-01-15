a2f8458de341f0620e7bf19276b2cb475db39ae0
jaimg <jaimg@LAPTOP-QMGK7OVA>
2020-01-15 13:56:55 +0100

    Inicio

---
+++ Ramas/src/main/java/com/mycompany/ramas/Ramas.java
@@ -0,0 +1,21 @@
+package com.mycompany.ramas;
+
+/**
+ *
+ * @author jaimg
+ */
+public class Ramas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola!");
+        
+    }
+    
+}
+
+
+
